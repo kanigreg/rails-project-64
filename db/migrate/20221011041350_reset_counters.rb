1ccb6f7786bfb9da8cd82666d03d31657b0e8c08
kanigreg <kanigreg@gmail.com>
2022-10-11 11:13:11 +0500

    Fix migration

--- db/migrate/20221011041350_reset_counters.rb
+++ db/migrate/20221011041350_reset_counters.rb
@@ -1,5 +1,5 @@
 class ResetCounters < ActiveRecord::Migration[6.1]
   def up
-    Post.ids { |id| Post.reset_counters(id, :comments, :likes) }
+    Post.ids.each { |id| Post.reset_counters(id, :comments, :likes) }
   end
 end
